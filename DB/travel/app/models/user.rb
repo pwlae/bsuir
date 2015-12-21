8e8fd76142fb6e9328a6dfb260116f97540d04fc
Pavel Danelian <Pavel_Danelian@epam.com>
2015-12-21 15:39:09 +0300

    Comments added

--- DB/travel/app/models/user.rb
+++ DB/travel/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 	validates :password, presence: true,
-            lenth: { minimum: 8 }
-	validates :email, presence: true					
+            length: { minimum: 8 }
+	validates :email, presence: true
 end
