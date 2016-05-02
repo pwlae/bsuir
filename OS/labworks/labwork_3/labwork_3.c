c0afd71f603eab912f2e4366fa038edfd76f4cab
Pavel Danelian <Pavel_Danelian@epam.com>
2016-05-02 17:18:28 +0300

    labwork_3 in development

--- OS/labworks/labwork_3/labwork_3.c
+++ OS/labworks/labwork_3/labwork_3.c
@@ -8,36 +8,36 @@ int get_checksum (char* file_path)
   FILE *fp;
   char buff[255];
 
-	// Define checksum variable
-	unsigned char checksum = 0;
+  // Define checksum variable
+  unsigned char checksum = 0;
 
   fp = fopen(file_path, "r");
 
-	while (!feof(fp) && !ferror(fp)) {
-		checksum ^= fgetc(fp);
-	}
-	return checksum;
+  while (!feof(fp) && !ferror(fp)) {
+    checksum ^= fgetc(fp);
+  }
+  return checksum;
 }
 
 int get_full_path (char* file_path)
 {
-	DIR *d;
-	struct dirent *dir;
-	d = opendir(file_path);
-	if (d)
-	{
-		while ((dir = readdir(d)) != NULL)
-		{
-			printf("%s\n", dir->d_name);
-		}
-		closedir(d);
-	}
+  DIR *d;
+  struct dirent *dir;
+  d = opendir(file_path);
+  if (d)
+  {
+    while ((dir = readdir(d)) != NULL)
+    {
+      printf("%s\n", dir->d_name);
+    }
+    closedir(d);
+  }
 }
 
 int main (int argc, char *argv[])
 {
-	//get_checksum(argv[1]);
+  //get_checksum(argv[1]);
 
-	printf ("%u\n", get_checksum(argv[1]));
+  printf ("%u\n", get_checksum(argv[1]));
   return(0);
 }
