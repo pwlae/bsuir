566d4e9f90a2c94c814c99417ecab4a21f7b521d
Pavel Danelian <Pavel_Danelian@epam.com>
2016-09-08 09:06:55 +0300

    TWOP initial

---
+++ TWOP/labwork_1.rb
@@ -0,0 +1,21 @@
+class Labwork1
+  def initialize
+		@tree = rand(1..10)
+	end
+
+  def add(*item)
+		#
+	end
+
+	def delete
+	end
+
+	def edit
+	end
+
+	private
+	def
+	end
+end
+
+labwork_1 = Labwork1.new
