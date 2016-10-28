cb836116fd69032852e468b46fe0fa3d0d30ecff
Pavel Danelian <Pavel_Danelian@epam.com>
2016-10-28 12:25:48 +0300

    labwork_1 fix

--- ОегрС/labwork_1/labwork_1.rb
+++ ОегрС/labwork_1/labwork_1.rb
@@ -10,36 +10,61 @@ module LabWork
     end
   end
 
+  class Dictionary
+    def initialize
+      #FIXME: Path should not be static
+      @dictionary = File.read('/Users/pdanelian/Desktop/dict_ru.txt')
+    end
+
+    def word_list
+      list = []
+      @dictionary.split("\n").each do |word|
+        list << word.split('|')[0].tr(' ', '')
+      end
+
+      list
+    end
+
+    def word_body
+      body = []
+      @dictionary.split("\n").each do |scheme|
+        # get body of each word
+        body << scheme.split('|')[1].tr(' ', '').split('/')[0]
+      end
+
+      body
+    end
+  end
+
+=begin
   class Database < Core
     def initialize
       @client = Mysql2::Client.new(
         hostname: 'localhost',
-        username: 'pdanelian',
-        database: 'test'
+        username: 'root',
+        database: 'words'
       )
     end
 
     def select_query
+      @client.query(query)
+
     end
   end
-
-  class Text < Core
-    def initialize
-    end
-
-    def to_array
-    end
-  end
+=end
 end
 
 class Text
   # TODO: test project
+
+  include LabWork
+
   def initialize
-    @text = File.read('/Users/pdanelian/Documents/changelog.txt')
+    @text = File.read('output')
     @text_array = []
   end
 
-  def counter
+  def word_counter
     word_counter = Hash.new 0
     text_to_array.each do |word|
       word_counter[word] += 1
@@ -51,6 +76,24 @@ class Text
     # p text_array_without_duplicates.length
   end
 
+  def uniq_word
+    uniq_word_list = []
+    word_counter = Hash.new 0
+    text_to_array.each do |word|
+      word_counter[word] += 1
+    end
+
+    word_counter.sort_by do |key, value|
+      uniq_word_list << key
+    end
+
+    uniq_word_list
+  end
+
+  # def synonym()
+  #   Database.new.select_query
+  # end
+
   def text_to_array(text = @text)
     text.split("\n").each do |line|
       line.split.join(' ').split(' ').each do |word|
@@ -59,6 +102,17 @@ class Text
     end
     @text_array
   end
+
+  def word_with_same_body
+    Dictionary.new.word_body.each do |body|
+      uniq_word.each do |word|
+        if /#{body}/ =~ word
+          p word
+        end
+      end
+    end
+  end
 end
 
-Text.new.counter
+Text.new.word_counter
+Text.new.word_with_same_body
